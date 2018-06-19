f8f35d43ff632199fac6a5eac9bedae4fd1db954
Priscila Oliveira <priscilawebdev@gmail.com>
2018-06-20 01:46:54 +0200

    feat: button component

---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { bm } from 'utils/bem'
+
+interface IButtonProps {
+  type?: string
+  label: string
+  fullWidth?: boolean
+  primary?: boolean
+  center?: boolean
+  className?: string[]
+  onClick: (event: React.FormEvent<HTMLButtonElement>) => void
+}
+
+const Button = ({
+  type = 'text',
+  fullWidth = false,
+  primary = false,
+  center = false,
+  label,
+  onClick,
+  className = []
+}: IButtonProps) => (
+  <button
+    className={bm('Button', { primary, center, fullWidth }, className)}
+    type={type}
+    onClick={onClick}
+  >
+    {label}
+  </button>
+)
+
+export default Button
