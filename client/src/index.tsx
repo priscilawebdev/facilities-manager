0f7da7c679663496841dd46af70b41f101517da3
oliveirap <priscila.oliveira@semantic-web.com>
2018-06-20 09:11:12 +0200

    fix: fixed missed imports

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+import Spinner from 'components/Spinner'
 import App from 'containers/App'
 import 'utils/styles/index.sass'
+import { store, persistor } from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Main = () => (
+    <Provider store={store}>
+        <PersistGate loading={<Spinner />} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
+)
+
+ReactDOM.render(<Main />, document.getElementById('root'))
