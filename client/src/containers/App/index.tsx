4750b563a4dc2dfa9364fa00f796bc46d762aa52
Priscila Oliveira <priscilawebdev@gmail.com>
2018-06-20 03:52:23 +0200

    refactor: changed method to pass id route

--- client/src/containers/App/index.tsx
+++ client/src/containers/App/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import FacilityList from 'containers/FacilityList'
-import FacilityDetail from 'containers/FacilityDetails'
+import FacilityDetails from 'containers/FacilityDetails'
 
 const App = () => (
   <Router>
     <Switch>
-      <Route path='/' component={FacilityList} />
-      <Route path='/:id' component={FacilityDetail} />
+      <Route exact path='/' component={FacilityList} />
+      <Route path='/details/:id' render={({match}) => (
+        <FacilityDetails id={match.params.id} />
+      )}/>
     </Switch>
   </Router>
 )
